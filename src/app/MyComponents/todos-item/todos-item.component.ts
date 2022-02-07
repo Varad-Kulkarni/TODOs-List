d5c38b9ca4d61d802eca0306c04b79a2504fe34a
unknown <kulkarnivarad110@gmail.com>
2022-02-07 11:25:18 +0530

    TODOS List

---
+++ src/app/MyComponents/todos-item/todos-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todos-item',
+  templateUrl: './todos-item.component.html',
+  styleUrls: ['./todos-item.component.css']
+})
+export class TodosItemComponent implements OnInit {
+
+  @Input()
+  todo!: Todo;
+  @Input()
+  i!: number;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckBox: EventEmitter<Todo> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(todo : Todo){
+    this.todoDelete.emit(todo);
+    console.log("Onclick is triggered");
+  }
+
+  onCheckBoxClick(todo: Todo){
+    this.todoCheckBox.emit(todo)
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodosItemComponent } from './MyComponents/todos-item/todos-item.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './MyComponents/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodosItemComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
